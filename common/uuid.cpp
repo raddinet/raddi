729bce5e4fc8e7a49f8ca6f1436dd8d27110f50d
raddinet <dev@raddi.net>
2018-07-29 04:41:08 +0200

    added super simple uuid

---
+++ common/uuid.cpp
@@ -0,0 +1,39 @@
+#include "uuid.h"
+#include <windows.h>
+#include <rpc.h>
+
+// simply using what Windows have to offer
+
+uuid::uuid () {
+    UuidCreate ((UUID *) this->data);
+}
+bool uuid::parse (const char * string) {
+    if (*string == '{') {
+        ++string;
+    }
+    return UuidFromStringA ((RPC_CSTR) string, (UUID *) this->data) == RPC_S_OK;
+}
+bool uuid::parse (const wchar_t * string) {
+    if (*string == L'{') {
+        ++string;
+    }
+    return UuidFromStringW ((RPC_WSTR) string, (UUID *) this->data) == RPC_S_OK;
+}
+std::string uuid::c_cstr () const {
+    RPC_CSTR rpcs;
+    std::string result;
+    if (UuidToStringA ((UUID *) this->data, &rpcs) == RPC_S_OK) {
+        result.assign (rpcs, rpcs + std::strlen ((char *) rpcs));
+        RpcStringFreeA (&rpcs);
+    }
+    return result;
+}
+std::wstring uuid::c_wstr () const {
+    RPC_WSTR rpcs;
+    std::wstring result;
+    if (UuidToStringW ((UUID *) this->data, &rpcs) == RPC_S_OK) {
+        result.assign (rpcs, rpcs + std::wcslen ((wchar_t *) rpcs));
+        RpcStringFreeW (&rpcs);
+    }
+    return result;
+}
---
+++ common/uuid.h
@@ -0,0 +1,39 @@
+#ifndef RADDI_UUID_H
+#define RADDI_UUID_H
+
+#include <cstddef>
+#include <string>
+#include "log.h"
+
+// uuid
+//  - simple UUID usable as map key
+//
+class uuid {
+    std::uint8_t data [16];
+
+public:
+    uuid ();
+    uuid (const uuid &) = default;
+
+    inline bool operator == (const uuid & other) const { return std::memcmp (this->data, other.data, 16) == 0; }
+    inline bool operator != (const uuid & other) const { return std::memcmp (this->data, other.data, 16) != 0; }
+    inline bool operator <  (const uuid & other) const { return std::memcmp (this->data, other.data, 16) <  0; }
+    inline bool operator <= (const uuid & other) const { return std::memcmp (this->data, other.data, 16) <= 0; }
+    inline bool operator >  (const uuid & other) const { return std::memcmp (this->data, other.data, 16) >  0; }
+    inline bool operator >= (const uuid & other) const { return std::memcmp (this->data, other.data, 16) >= 0; }
+
+    bool parse (const char *);
+    bool parse (const wchar_t *);
+
+    std::string c_cstr () const;
+    std::wstring c_wstr () const;
+};
+
+// translate
+//  - for passing uuid as a log function parameter
+//
+inline std::wstring translate (uuid v, const std::wstring &) {
+    return v.c_wstr ();
+}
+
+#endif
