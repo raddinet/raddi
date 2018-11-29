b03e1782342e7a12f42e1a068d1e7e88a635f58f
raddinet <dev@raddi.net>
2018-11-29 02:22:07 +0100

    TODO for installer moved to installer.cpp

--- installer/installer.cpp
+++ installer/installer.cpp
@@ -21,7 +21,50 @@
 #include "../common/threadpool.h"
 
 int CALLBACK wWinMain (HINSTANCE hInstance, HINSTANCE, LPWSTR, int nCmdShow) {
-    // TODO
+    
+    // detect MSVC runtime version
+
+    // detect if already installed (and what version)
+    //  - if payload contains higher version, display "update" button and "install" (either "for me" or "system")
+    //  - show "change/repair" button (future?)
+    //     - mode to backup 
+    //  - show "uninstall" button
+
+    // Wizard:
+    //  - NO hello page: message "just click next/next/next" on first page
+    //  - service/tray/on demand (app starts node)
+    //     - radio buttons with short explanation
+    //     - default: service if elevated, tray otherwise
+    //  - role
+    //     - core / normal (default) / leaf
+    //  - installation path:
+    //     - if elevated suggest "program files", otherwise users appdata (yeah, sorry)
+    //     - if directed to unwrittable path, restart itself elevated (remember choices)
+    //     - if not present, and not elevated, ask user to install portable build?
+    //  - shortcuts:
+    //     - start menu (checked)
+    //     - desktop (unchecked)
+    //     - quicklaunch (show on XP ...and vista? ...checked if not installed as tray)
+    //     - for all users / just for me
+    //        - display only if elevated and installing service
+    //  - done page (returned from elevated process (if any)):
+    //     - checkbox to run the app
+
+    // Process:
+    //  - msvc runtime
+    //     - if redist exe present, and elevated, run redist install (or update) / install / passive / norestart
+    //     - if not present, redirect user to download if not installed
+    //     - if not present, and not elevated, install portable build (redirect to download?)
+    //     - warn if very old?
+
+    //  - if overwriting running executable, shut it down
+    //     - stop service or stop background task (tray?)
+
+    //  - decompress to selected location
+    //     - DO NOT OVERWRITE LOCAL raddi.db IF EXISTS (actually app itself should deploy/create the default one)
+    //  - create service or autorun tray icon carrier process
+    //  - add raddiXX.exe to firewall (cmd
+    
     return 0;
 }
 
