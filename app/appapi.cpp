22b15c74f7f36d30f97d914b82ee3bdd2fe2799f
raddinet <dev@raddi.net>
2019-02-25 17:23:05 +0100

    extracted optional Windows API functions retrieval and some general operations

---
+++ app/appapi.cpp
@@ -0,0 +1,272 @@
+#include "appapi.h"
+#include <VersionHelpers.h>
+
+HRESULT (WINAPI * ptrBufferedPaintInit) () = NULL;
+HRESULT (WINAPI * ptrBufferedPaintSetAlpha) (HPAINTBUFFER, const RECT *, BYTE) = NULL;
+HPAINTBUFFER (WINAPI * ptrBeginBufferedPaint) (HDC, const RECT *, BP_BUFFERFORMAT, const BP_PAINTPARAMS *, HDC *) = NULL;
+HRESULT (WINAPI * ptrEndBufferedPaint) (HPAINTBUFFER, BOOL) = NULL;
+HDC (WINAPI * ptrGetBufferedPaintDC) (HPAINTBUFFER) = NULL;
+HRESULT (WINAPI * ptrGetBufferedPaintBits) (HPAINTBUFFER, RGBQUAD **, int *) = NULL;
+HRESULT (WINAPI * ptrDrawThemeTextEx) (HTHEME, HDC, int, int, LPCWSTR, int, DWORD, LPRECT, const DTTOPTS *) = NULL;
+
+BOOL (WINAPI * ptrAllowDarkModeForApp) (BOOL) = NULL;
+BOOL (WINAPI * ptrAllowDarkModeForWindow) (HWND, BOOL) = NULL;
+void (WINAPI * ptrFlushMenuThemes) () = NULL;
+void (WINAPI * ptrRefreshImmersiveColorPolicyState) () = NULL;
+
+BOOL (WINAPI * ptrDwmDefWindowProc) (HWND, UINT, WPARAM, LPARAM, LRESULT *) = NULL;
+HRESULT (WINAPI * ptrDwmGetWindowAttribute) (HWND, DWORD, PVOID, DWORD) = NULL;
+HRESULT (WINAPI * ptrDwmSetWindowAttribute) (HWND, DWORD, LPCVOID, DWORD) = NULL;
+HRESULT (WINAPI * ptrDwmExtendFrameIntoClientArea) (HWND, const MARGINS *) = NULL;
+HRESULT (WINAPI * ptrDwmGetColorizationColor) (DWORD *, BOOL *) = NULL;
+HRESULT (WINAPI * ptrDwmIsCompositionEnabled) (BOOL *) = NULL;
+
+BOOL (WINAPI * ptrEnableNonClientDpiScaling) (HWND) = NULL;
+BOOL (WINAPI * ptrChangeWindowMessageFilter) (UINT, DWORD) = NULL;
+UINT (WINAPI * pfnGetDpiForSystem) () = NULL;
+UINT (WINAPI * pfnGetDpiForWindow) (HWND) = NULL;
+int (WINAPI * ptrGetSystemMetricsForDpi) (int, UINT) = NULL;
+DPI_AWARENESS_CONTEXT (WINAPI * ptrGetWindowDpiAwarenessContext) (HWND) = NULL;
+BOOL (WINAPI * ptrAreDpiAwarenessContextsEqual) (DPI_AWARENESS_CONTEXT, DPI_AWARENESS_CONTEXT) = NULL;
+
+HRESULT (WINAPI * ptrLoadIconWithScaleDown) (HINSTANCE, PCWSTR, int, int, HICON *) = NULL;
+HRESULT (WINAPI * ptrTaskDialogIndirect) (const TASKDIALOGCONFIG *, int * button, int * radio, BOOL * check) = NULL;
+
+void AppApiInitialize () {
+    if (HMODULE hUxTheme = GetModuleHandle (L"UXTHEME")) {
+        Symbol (hUxTheme, ptrBufferedPaintInit, "BufferedPaintInit");
+        Symbol (hUxTheme, ptrBeginBufferedPaint, "BeginBufferedPaint");
+        Symbol (hUxTheme, ptrBufferedPaintSetAlpha, "BufferedPaintSetAlpha");
+        Symbol (hUxTheme, ptrGetBufferedPaintBits, "GetBufferedPaintBits");
+        Symbol (hUxTheme, ptrGetBufferedPaintDC, "GetBufferedPaintDC");
+        Symbol (hUxTheme, ptrEndBufferedPaint, "EndBufferedPaint");
+        Symbol (hUxTheme, ptrDrawThemeTextEx, "DrawThemeTextEx");
+
+        if (IsWindowsBuildOrGreater (10, 0, 17763)) {
+            Symbol (hUxTheme, ptrFlushMenuThemes, 136);
+            Symbol (hUxTheme, ptrAllowDarkModeForApp, 135);
+            Symbol (hUxTheme, ptrAllowDarkModeForWindow, 133);
+            Symbol (hUxTheme, ptrRefreshImmersiveColorPolicyState, 104);
+        }
+    }
+    if (HMODULE hUser32 = GetModuleHandle (L"USER32")) {
+        Symbol (hUser32, ptrEnableNonClientDpiScaling, "EnableNonClientDpiScaling");
+        Symbol (hUser32, ptrChangeWindowMessageFilter, "ChangeWindowMessageFilter");
+
+        Symbol (hUser32, pfnGetDpiForSystem, "GetDpiForSystem");
+        Symbol (hUser32, pfnGetDpiForWindow, "GetDpiForWindow");
+        Symbol (hUser32, ptrGetSystemMetricsForDpi, "GetSystemMetricsForDpi");
+        Symbol (hUser32, ptrGetWindowDpiAwarenessContext, "GetWindowDpiAwarenessContext");
+        Symbol (hUser32, ptrAreDpiAwarenessContextsEqual, "AreDpiAwarenessContextsEqual");
+    }
+    if (HMODULE hComCtl32 = GetModuleHandle (L"COMCTL32")) {
+        Symbol (hComCtl32, ptrLoadIconWithScaleDown, "LoadIconWithScaleDown");
+        Symbol (hComCtl32, ptrTaskDialogIndirect, "TaskDialogIndirect");
+    }
+    if (HMODULE hDwmApi = LoadLibrary (L"DWMAPI")) {
+        Symbol (hDwmApi, ptrDwmDefWindowProc, "DwmDefWindowProc");
+        Symbol (hDwmApi, ptrDwmGetWindowAttribute, "DwmGetWindowAttribute");
+        Symbol (hDwmApi, ptrDwmSetWindowAttribute, "DwmSetWindowAttribute");
+        Symbol (hDwmApi, ptrDwmIsCompositionEnabled, "DwmIsCompositionEnabled");
+        Symbol (hDwmApi, ptrDwmGetColorizationColor, "DwmGetColorizationColor");
+        Symbol (hDwmApi, ptrDwmExtendFrameIntoClientArea, "DwmExtendFrameIntoClientArea");
+    }
+}
+
+POINT GetDPI (HWND hWnd) {
+    if (hWnd != NULL) {
+        if (pfnGetDpiForWindow) {
+            LONG dpi = pfnGetDpiForWindow (hWnd);
+            return { dpi, dpi };
+        }
+    } else {
+        if (pfnGetDpiForSystem) {
+            LONG dpi = pfnGetDpiForSystem ();
+            return { dpi, dpi };
+        }
+    }
+    if (HDC hDC = GetDC (hWnd)) {
+        POINT dpi = {
+            GetDeviceCaps (hDC, LOGPIXELSX),
+            GetDeviceCaps (hDC, LOGPIXELSY)
+        };
+        ReleaseDC (hWnd, hDC);
+        return dpi;
+    } else
+        return { 96, 96 };
+}
+
+RECT FixWindowCoordinates (int x, int y, int w, int h) {
+    if ((w > 0) && (h > 0)) {
+        RECT r = { x, y, x + w, y + h };
+
+        auto monitor = MonitorFromRect (&r, MONITOR_DEFAULTTONULL);
+        if (monitor != NULL) {
+
+            MONITORINFO info;
+            info.cbSize = sizeof info;
+
+            if (GetMonitorInfo (monitor, &info)) {
+                RECT rX;
+                if (IntersectRect (&rX, &r, &info.rcWork)) {
+
+                    // use the provided coordinates if at least 1/16 of window is visible
+
+                    if (((rX.right - rX.left) * (rX.bottom - rX.top)) >= (w * h) / 16) {
+                        return { x, y, w, h };
+                    }
+                }
+            }
+        }
+    }
+    return { CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT };
+}
+
+bool IsLastWindow (HWND hWnd) {
+    auto atom = GetClassLongPtr (hWnd, GCW_ATOM);
+    return FindWindowEx (NULL, hWnd, (LPCTSTR) atom, NULL) == NULL
+        && FindWindowEx (NULL, NULL, (LPCTSTR) atom, NULL) == hWnd;
+}
+
+HICON LoadBestIcon (HMODULE hModule, LPCWSTR resource, SIZE size) {
+    HICON hNewIcon = NULL;
+    if (size.cx > 256) size.cx = 256;
+    if (size.cy > 256) size.cy = 256;
+
+    if (ptrLoadIconWithScaleDown) {
+        if (ptrLoadIconWithScaleDown (hModule, resource, size.cx, size.cy, &hNewIcon) != S_OK) {
+            hNewIcon = NULL;
+        }
+    }
+    if (hNewIcon)
+        return hNewIcon;
+    else
+        return (HICON) LoadImage (hModule, resource, IMAGE_ICON, size.cx, size.cy, LR_DEFAULTCOLOR);
+}
+
+HBRUSH CreateSolidBrushEx (COLORREF color, unsigned char alpha) {
+    auto b = alpha * ((color & 0x00FF0000) >> 16);
+    auto g = alpha * ((color & 0x0000FF00) >> 8);
+    auto r = alpha * ((color & 0x000000FF) >> 0);
+
+    BITMAPINFO bi = {
+        {   sizeof (BITMAPINFO),
+            1,1,1, 32,BI_RGB, 0,0,0,0,0
+        }, {
+            (BYTE) ((b * 0x8081) >> 0x17), // divide by 255
+            (BYTE) ((g * 0x8081) >> 0x17),
+            (BYTE) ((r * 0x8081) >> 0x17),
+            alpha
+        }
+    };
+    return CreateDIBPatternBrushPt (&bi, DIB_RGB_COLORS);
+}
+HBRUSH CreateSolidBrushEx (COLORREF color) {
+    return CreateSolidBrushEx (color, unsigned (color) >> 24u);
+}
+bool IsColorDark (COLORREF color) {
+    return 2 * GetRValue (color) + 5 * GetGValue (color) + GetBValue (color) <= 1024; // MS default
+    // return 299 * GetRValue (color) + 587 * GetGValue (color) + 114 * GetBValue (color) <= 128000; // YIQ?
+}
+
+HRESULT DrawCompositedText (HDC hDC, HTHEME hTheme, HFONT hFont,
+                            LPCWSTR string, int length, DWORD format, RECT r,
+                            COLORREF color, UINT glow) {
+    SelectObject (hDC, hFont);
+
+    DTTOPTS options;
+    options.dwSize = sizeof options;
+    options.dwFlags = DTT_COMPOSITED | DTT_TEXTCOLOR;
+    options.crText = color;
+
+    if (glow) {
+        options.dwFlags |= DTT_GLOWSIZE;
+        options.iGlowSize = glow;
+    }
+    return ptrDrawThemeTextEx (hTheme, hDC, 0, 0,
+                               string, length, format & ~(DT_MODIFYSTRING | DT_CALCRECT),
+                               &r, &options);
+}
+
+void Cursors::update () {
+    this->wait = LoadCursor (NULL, IDC_WAIT);
+    this->arrow = LoadCursor (NULL, IDC_ARROW);
+    this->working = LoadCursor (NULL, IDC_APPSTARTING);
+    this->vertical = LoadCursor (NULL, IDC_SIZENS);
+    this->horizontal = LoadCursor (NULL, IDC_SIZEWE);
+}
+void Design::update () {
+    if (ptrRefreshImmersiveColorPolicyState) {
+        ptrRefreshImmersiveColorPolicyState ();
+    }
+    if (ptrFlushMenuThemes) {
+        ptrFlushMenuThemes ();
+    }
+
+    BOOL compositionEnabled = FALSE;
+    if (ptrDwmIsCompositionEnabled) {
+        ptrDwmIsCompositionEnabled (&compositionEnabled);
+    }
+
+    this->composited = compositionEnabled;
+    this->nice = (this->composited || (IsAppThemed () && IsWindowsVistaOrGreater ()));
+    this->alpha = this->composited && !IsWindows10OrGreater ();
+
+    if (ptrDwmGetColorizationColor) {
+        BOOL opaque = TRUE;
+        DWORD color;
+        if (ptrDwmGetColorizationColor (&color, &opaque) == S_OK) {
+            this->colorization.active = RGB (GetBValue (color), GetGValue (color), GetRValue (color));
+        } else {
+            this->colorization.active = GetSysColor (COLOR_WINDOW);
+        }
+        this->colorization.inactive = this->colorization.active;
+    }
+
+    HIGHCONTRAST hc;
+    hc.cbSize = sizeof hc;
+    if (SystemParametersInfo (SPI_GETHIGHCONTRAST, sizeof hc, &hc, 0)) {
+        this->contrast = hc.dwFlags & HCF_HIGHCONTRASTON;
+    }
+
+    wchar_t filename [MAX_PATH];
+    if (GetCurrentThemeName (filename, MAX_PATH, NULL, 0, NULL, 0) == S_OK) {
+        if (std::wcsstr (filename, L"AeroLite.msstyles")) {
+            this->contrast = true;
+        }
+    }
+
+    if (this->contrast) {
+        this->colorization.active = GetSysColor (COLOR_ACTIVECAPTION);
+        this->colorization.inactive = GetSysColor (COLOR_INACTIVECAPTION);
+        this->prevalence = true;
+    } else
+    if (IsWindows10OrGreater ()) {
+        this->prevalence = false;
+        this->colorization.active = 0xFFFFFF;
+        this->colorization.inactive = 0xFFFFFF;
+
+        HKEY hKey;
+        if (RegOpenKeyEx (HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Windows\\DWM", 0, KEY_QUERY_VALUE, &hKey) == ERROR_SUCCESS) {
+            DWORD value = TRUE;
+            DWORD size = sizeof value;
+            RegQueryValueEx (hKey, L"AccentColor", NULL, NULL, reinterpret_cast <LPBYTE> (&this->colorization.active), &size);
+            RegQueryValueEx (hKey, L"AccentColorInactive", NULL, NULL, reinterpret_cast <LPBYTE> (&this->colorization.inactive), &size);
+            if (RegQueryValueEx (hKey, L"ColorPrevalence", NULL, NULL, reinterpret_cast <LPBYTE> (&value), &size) == ERROR_SUCCESS) {
+                this->prevalence = value;
+            }
+            RegCloseKey (hKey);
+        }
+
+        if (ptrAllowDarkModeForWindow) {
+            if (RegOpenKeyEx (HKEY_CURRENT_USER, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", 0, KEY_QUERY_VALUE, &hKey) == ERROR_SUCCESS) {
+                DWORD value = TRUE;
+                DWORD size = sizeof value;
+                if (RegQueryValueEx (hKey, L"AppsUseLightTheme", NULL, NULL, reinterpret_cast <LPBYTE> (&value), &size) == ERROR_SUCCESS) {
+                    this->light = value;
+                }
+                RegCloseKey (hKey);
+            }
+        }
+    }
+}
---
+++ app/appapi.h
@@ -0,0 +1,90 @@
+#ifndef APPAPI_H
+#define APPAPI_H
+
+#include <windows.h>
+#include <uxtheme.h>
+
+#include "../common/platform.h"
+
+void AppApiInitialize (); // loads ptrAbcXyz functions below
+
+POINT GetDPI (HWND hWnd);
+HICON LoadBestIcon (HMODULE hModule, LPCWSTR resource, SIZE size);
+HBRUSH CreateSolidBrushEx (COLORREF color);
+HBRUSH CreateSolidBrushEx (COLORREF color, unsigned char alpha);
+RECT FixWindowCoordinates (int x, int y, int w, int h);
+bool IsLastWindow (HWND hWnd);
+bool IsColorDark (COLORREF color);
+
+HRESULT DrawCompositedText (HDC, HTHEME, HFONT, LPCWSTR, int, DWORD, RECT, COLORREF, UINT);
+
+struct Design {
+    bool composited = true; // DWM composition is enabled
+    bool nice = false; // DWM or theme on Vista/7
+    bool light = true;
+    bool prevalence = true;
+    bool contrast = false;
+    bool alpha = false; // need to fix alpha on glass
+
+    struct {
+        DWORD active = 0xFFFFFF;
+        DWORD inactive = 0xFFFFFF;
+    } colorization;
+
+    void update ();
+};
+
+struct Cursors {
+    HCURSOR wait = NULL;
+    HCURSOR arrow = NULL;
+    HCURSOR working = NULL;
+    HCURSOR vertical = NULL;
+    HCURSOR horizontal = NULL;
+
+    void update ();
+};
+
+template <unsigned int N>
+int TranslateAccelerators (HWND hWnd, const HACCEL (&table) [N], LPMSG msg);
+
+template <unsigned int N>
+int TranslateAccelerators (HWND hWnd, const HACCEL (&table) [N], LPMSG msg) {
+    for (auto accelerator : table) {
+        if (TranslateAccelerator (hWnd, accelerator, msg))
+            return true;
+    }
+    return false;
+}
+
+extern HRESULT (WINAPI * ptrBufferedPaintInit) ();
+extern HRESULT (WINAPI * ptrBufferedPaintSetAlpha) (HPAINTBUFFER, const RECT *, BYTE);
+extern HPAINTBUFFER (WINAPI * ptrBeginBufferedPaint) (HDC, const RECT *, BP_BUFFERFORMAT, const BP_PAINTPARAMS *, HDC *);
+extern HRESULT (WINAPI * ptrEndBufferedPaint) (HPAINTBUFFER, BOOL);
+extern HDC (WINAPI * ptrGetBufferedPaintDC) (HPAINTBUFFER);
+extern HRESULT (WINAPI * ptrGetBufferedPaintBits) (HPAINTBUFFER, RGBQUAD **, int *);
+extern HRESULT (WINAPI * ptrDrawThemeTextEx) (HTHEME, HDC, int, int, LPCWSTR, int, DWORD, LPRECT, const DTTOPTS *);
+
+extern BOOL (WINAPI * ptrAllowDarkModeForApp) (BOOL);
+extern BOOL (WINAPI * ptrAllowDarkModeForWindow) (HWND, BOOL);
+extern void (WINAPI * ptrFlushMenuThemes) ();
+extern void (WINAPI * ptrRefreshImmersiveColorPolicyState) ();
+
+extern BOOL (WINAPI * ptrDwmDefWindowProc) (HWND, UINT, WPARAM, LPARAM, LRESULT *);
+extern HRESULT (WINAPI * ptrDwmGetWindowAttribute) (HWND, DWORD, PVOID, DWORD);
+extern HRESULT (WINAPI * ptrDwmSetWindowAttribute) (HWND, DWORD, LPCVOID, DWORD);
+extern HRESULT (WINAPI * ptrDwmExtendFrameIntoClientArea) (HWND, const MARGINS *);
+extern HRESULT (WINAPI * ptrDwmGetColorizationColor) (DWORD *, BOOL *);
+extern HRESULT (WINAPI * ptrDwmIsCompositionEnabled) (BOOL *);
+
+extern BOOL (WINAPI * ptrEnableNonClientDpiScaling) (HWND);
+extern BOOL (WINAPI * ptrChangeWindowMessageFilter) (UINT, DWORD);
+extern UINT (WINAPI * pfnGetDpiForSystem) ();
+extern UINT (WINAPI * pfnGetDpiForWindow) (HWND);
+extern int (WINAPI * ptrGetSystemMetricsForDpi) (int, UINT);
+extern DPI_AWARENESS_CONTEXT (WINAPI * ptrGetWindowDpiAwarenessContext) (HWND);
+extern BOOL (WINAPI * ptrAreDpiAwarenessContextsEqual) (DPI_AWARENESS_CONTEXT, DPI_AWARENESS_CONTEXT);
+
+extern HRESULT (WINAPI * ptrLoadIconWithScaleDown) (HINSTANCE, PCWSTR, int, int, HICON *);
+extern HRESULT (WINAPI * ptrTaskDialogIndirect) (const TASKDIALOGCONFIG *, int * button, int * radio, BOOL * check);
+
+#endif
