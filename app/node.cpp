89b49829411e54526a244b06a2b05212a50deba0
raddinet <dev@raddi.net>
2019-04-25 18:22:26 +0200

    keep app connected to db/node

--- app/node.cpp
+++ app/node.cpp
@@ -5,17 +5,24 @@ namespace {
     HANDLE thread = NULL;
 
     template <typename C, long (C::*fn) ()>
-    DWORD WINAPI forward (LPVOID lpVoid) {
+    DWORD WINAPI forward (LPVOID lpVoid) noexcept {
         return (static_cast <C *> (lpVoid)->*fn) ();
     }
 }
 
-bool Node::initialize (const wchar_t * pid) {
-    iocp = CreateIoCompletionPort (INVALID_HANDLE_VALUE, NULL, 0, 0);
-    thread = CreateThread (NULL, 0, forward <Node, &Node::worker>, this, 0, NULL);
+bool Node::initialize (const wchar_t * pid, DWORD message) {
+    if (iocp == NULL) {
+        iocp = CreateIoCompletionPort (INVALID_HANDLE_VALUE, NULL, 0, 0);
+    }
+    if (thread == NULL) {
+        thread = CreateThread (NULL, 0, forward <Node, &Node::worker>, this, 0, NULL);
+    }
 
     if (iocp && thread) {
         this->parameter = pid;
+        this->guiMessage = message;
+        this->guiThreadId = GetCurrentThreadId ();
+        this->report (raddi::log::level::note, 0x20, this->guiThreadId, this->parameter);
         return true;
     } else
         return false;
@@ -41,63 +48,90 @@ bool Node::connected () const {
         auto heartbeat = this->instance->get <unsigned long long> (L"heartbeat");
         auto now = raddi::microtimestamp ();
 
-        return (now - heartbeat) < (3 * 1'000'000uLL);
+        return !raddi::older (heartbeat, now - 3 * 1'000'000uLL);
     } else
         return false;
 }
 
-long Node::worker () {
+long Node::worker () noexcept {
+    bool         previous = false;
     BOOL         success;
     DWORD        n = 0;
     ULONG_PTR    key = 0;
     OVERLAPPED * overlapped = NULL;
 
     do {
-        success = GetQueuedCompletionStatus (iocp, &n, &key, &overlapped, 1000);
+        success = GetQueuedCompletionStatus (iocp, &n, &key, &overlapped, 250);
         if (overlapped) {
             try {
                 static_cast <Overlapped *> (overlapped)->completion (success, n);
 
-            } catch (const std::exception & x) {
-                //raddi::log::error (6, i, id, x.what ());
-            } catch (...) {
-                //raddi::log::stop (7, i, id);
-                PostQueuedCompletionStatus (iocp, 0, 0, NULL);
-            }
-        }
-
-        if (!success && (GetLastError () == WAIT_TIMEOUT)) {
-
-            // log
-
-            if (!this->connected ()) {
-                delete this->database;
-                delete this->instance;
-
-                this->database = nullptr;
-                this->instance = nullptr;
-
-                try {
-                    this->instance = new raddi::instance (this->parameter);
-                    this->database = new raddi::db (file::access::read, this->instance->get <std::wstring> (L"database"));
-
-                } catch (...) {
-
-                }
-                // reconnect
-
                 // TODO: notify GUI thread (views) on change to tables
-                // TODO: process monitor changes (and other awaits) on this thread
+                // TODO: how to determine which table has changed (add db::table callback?)
+
+            } catch (const std::bad_alloc & x) {
+                this->report (raddi::log::level::error, 0x21, x.what ());
+                PostThreadMessage (this->guiThreadId, this->guiMessage, 1, ERROR_NOT_ENOUGH_MEMORY);
+            } catch (const std::exception & x) {
+                this->report (raddi::log::level::error, 0x21, x.what ());
+                PostThreadMessage (this->guiThreadId, this->guiMessage, 1, 0);
+            } catch (...) {
+                this->report (raddi::log::level::error, 0x20);
+                PostThreadMessage (this->guiThreadId, this->guiMessage, 1, 0);
+                Sleep (10);
+            }
+        } else
+        if (!success && (GetLastError () == WAIT_TIMEOUT)) {
+            n = 1; // continue loop
+        }
+
+        if (!this->connected ()) {
+            this->disconnect ();
+            if (previous) {
+                previous = false;
+                this->report (raddi::log::level::note, 0x22);
+                PostThreadMessage (this->guiThreadId, this->guiMessage, 0, FALSE);
+            }
+            if (this->reconnect ()) {
+                this->report (raddi::log::level::note, 0x21);
+                PostThreadMessage (this->guiThreadId, this->guiMessage, 0, TRUE);
+                previous = true;
             }
         }
+        
     } while (overlapped || key || n);
 
-    // log
-
+    this->report (raddi::log::level::note, 0x2F);
+    this->disconnect ();
     return 0;
 }
 
-// glue async operations of RADDI library to worker thread running here
+bool Node::reconnect () {
+    try {
+        this->instance = new raddi::instance (parameter);
+        if (this->instance->status == ERROR_SUCCESS) {
+            this->database = new raddi::db (file::access::read, this->instance->get <std::wstring> (L"database"));
+
+            return this->database->connected ();
+        }
+    } catch (...) {
+    }
+    return false;
+}
+
+bool Node::disconnect () {
+    if (this->database || this->instance) {
+        delete this->database;
+        delete this->instance;
+
+        this->database = nullptr;
+        this->instance = nullptr;
+        return true;
+    } else
+        return false;
+}
+
+// glue async operations of RADDI core to worker thread running here
 
 bool Overlapped::await (HANDLE handle, void * key) noexcept {
     return CreateIoCompletionPort (handle, iocp, (ULONG_PTR) key, 0) == iocp;
--- app/node.h
+++ app/node.h
@@ -2,24 +2,34 @@
 #define NODE_H
 
 #include "../core/raddi.h"
+#include "../common/log.h"
 
 // Node
 //  - node connection for client app
-//  - singleton, mutiple instances 
+//  - singleton, mutiple instances will break!
+//  - thread that initialized the instance get
 //
-class Node {
-    const wchar_t *     parameter;
-    raddi::instance *   instance = nullptr;// (option (argc, argw, L"instance"));
-    raddi::db *         database = nullptr;// (file::access::read,
+class Node
+    : virtual raddi::log::provider <raddi::component::main> {
+
+    raddi::instance *   instance = nullptr; // (option (argc, argw, L"instance"));
+    raddi::db *         database = nullptr; // (file::access::read,
+    const wchar_t *     parameter = nullptr;
+    DWORD               guiThreadId = 0;
+    DWORD               guiMessage = 0;
 
 public:
-    bool initialize (const wchar_t * instance);
+    Node () : provider ("connection") {};
+
+    bool initialize (const wchar_t * instance, DWORD message);
     void terminate ();
 
     bool connected () const;
 
 private:
-    long worker ();
+    long worker () noexcept;
+    bool disconnect ();
+    bool reconnect ();
 };
 
 #endif
