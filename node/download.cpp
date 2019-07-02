dab4ab3a172d72fecd210d68a2db4758dcad16c3
raddinet <dev@raddi.net>
2019-07-02 21:43:46 +0200

    generalize the file download facility, for better reporting and reuse

--- node/download.cpp
+++ node/download.cpp
@@ -1,5 +1,4 @@
 #include "download.h"
-#include <winhttp.h>
 
 #include <cctype>
 #include <string>
@@ -7,16 +6,7 @@
 #include "../common/log.h"
 #include "../node/server.h"
 
-namespace {
-    HINTERNET internet = NULL;
-    struct DownloadDetails {
-        HINTERNET    connection;
-        std::wstring url;
-    };
-    void WINAPI HttpHandler (HINTERNET, DWORD_PTR, DWORD, LPVOID, DWORD);
-}
-
-bool InitializeDownload (const wchar_t * proxy, const wchar_t * user_agent) {
+Download::Download (const wchar_t * proxy, const wchar_t * user_agent) : provider ("downloader") {
     auto proxy_type = WINHTTP_ACCESS_TYPE_NO_PROXY;
     if (proxy) {
         if (proxy [0] == L'\0') {
@@ -27,22 +17,22 @@ bool InitializeDownload (const wchar_t * proxy, const wchar_t * user_agent) {
         }
     }
 
-    internet = WinHttpOpen (user_agent, proxy_type, proxy, WINHTTP_NO_PROXY_BYPASS, WINHTTP_FLAG_ASYNC);
-    if (internet) {
-        if (WinHttpSetStatusCallback (internet, HttpHandler,
+    this->internet = WinHttpOpen (user_agent, proxy_type, proxy, WINHTTP_NO_PROXY_BYPASS, WINHTTP_FLAG_ASYNC);
+    if (this->internet) {
+        if (WinHttpSetStatusCallback (internet, Download::Context::HttpHandlerFwd,
                                       WINHTTP_CALLBACK_FLAG_ALL_COMPLETIONS, NULL) != WINHTTP_INVALID_STATUS_CALLBACK) {
-            return true;
+            return;
         }
     }
-    return false;
+    throw raddi::log::exception (raddi::component::main, 10);
 }
 
-void TerminateDownload () {
-    WinHttpSetStatusCallback (internet, NULL, WINHTTP_CALLBACK_FLAG_ALL_NOTIFICATIONS, NULL);
-    WinHttpCloseHandle (internet);
+Download::~Download () {
+    WinHttpSetStatusCallback (this->internet, NULL, WINHTTP_CALLBACK_FLAG_ALL_NOTIFICATIONS, NULL);
+    WinHttpCloseHandle (this->internet);
 }
 
-void Download (wchar_t * url) {
+bool Download::download (wchar_t * url, Callback * callback) {
     URL_COMPONENTS components;
     std::memset (&components, 0, sizeof components);
     components.dwStructSize = sizeof components;
@@ -59,7 +49,7 @@ void Download (wchar_t * url) {
         components.lpszScheme [components.dwSchemeLength] = L'\0';
         components.lpszHostName [components.dwHostNameLength] = L'\0';
 
-        if (auto connection = WinHttpConnect (internet, components.lpszHostName, components.nPort, 0)) {
+        if (auto connection = WinHttpConnect (this->internet, components.lpszHostName, components.nPort, 0)) {
 
             components.lpszUrlPath [0] = L'/';
             if (auto request = WinHttpOpenRequest (connection, NULL, components.lpszUrlPath, NULL,
@@ -75,125 +65,116 @@ void Download (wchar_t * url) {
                                                 components.lpszUserName,
                                                 components.lpszPassword ? components.lpszPassword : L"",
                                                 NULL)) {
-                        raddi::log::error (raddi::component::main, 0x24);
+                        this->report (raddi::log::level::error, 0x24);
                     }
                 }
-                try {
-                    if (!WinHttpSendRequest (request, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0,
-                                             reinterpret_cast <DWORD_PTR> (new DownloadDetails {
-                                                 connection, components.lpszScheme + std::wstring (L"://") + components.lpszHostName
-                                                                           }))) {
-                        raddi::log::error (raddi::component::main, 0x23);
-                        WinHttpCloseHandle (request);
-                        WinHttpCloseHandle (connection);
-                    }
-                } catch (const std::bad_alloc & x) {
-                    raddi::log::error (5, -1, GetCurrentThreadId (), x.what ());
+
+                if (WinHttpSendRequest (request, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0,
+                                        reinterpret_cast <DWORD_PTR> (new Context (connection, callback,
+                                                                                   components.lpszScheme + std::wstring (L"://") + components.lpszHostName)))) {
+                    return true;
+
+                } else {
+                    this->report (raddi::log::level::error, 0x23);
+                    WinHttpCloseHandle (request);
+                    WinHttpCloseHandle (connection);
                 }
             } else {
-                raddi::log::error (raddi::component::main, 0x22, components.lpszHostName, (unsigned int) components.nPort, components.lpszUrlPath);
+                this->report (raddi::log::level::error, 0x22, components.lpszHostName, (unsigned int) components.nPort, components.lpszUrlPath);
                 WinHttpCloseHandle (connection);
             }
         } else
-            raddi::log::error (raddi::component::main, 0x21, components.lpszHostName, (unsigned int) components.nPort);
+            this->report (raddi::log::level::error, 0x21, components.lpszHostName, (unsigned int) components.nPort);
     } else
-        raddi::log::error (raddi::component::main, 0x20, url);
+        this->report (raddi::log::level::error, 0x20, url);
+
+    return false;
 }
 
-namespace {
-    void WINAPI HttpHandler (HINTERNET request, DWORD_PTR context_, DWORD code, LPVOID data_, DWORD size) {
-        auto context = reinterpret_cast <const DownloadDetails *> (context_);
-        char buffer [8193];
-        switch (code) {
+void Download::Context::HttpHandler (HINTERNET request, DWORD code, char * data, DWORD size) {
+    char buffer [8193];
+    switch (code) {
 
-            case WINHTTP_CALLBACK_STATUS_REQUEST_ERROR:
-            case WINHTTP_CALLBACK_STATUS_CLOSING_CONNECTION:
-                raddi::log::error (raddi::component::main, 0x26, context->url);
-                break;
+        case WINHTTP_CALLBACK_STATUS_REQUEST_ERROR:
+        case WINHTTP_CALLBACK_STATUS_CLOSING_CONNECTION:
+            this->report (raddi::log::level::error, 0x26, this->url);
+            break;
 
-                // successes return early here
-            case WINHTTP_CALLBACK_STATUS_SENDREQUEST_COMPLETE:
-                if (WinHttpReceiveResponse (request, NULL))
-                    return;
+            // successes return early here
+        case WINHTTP_CALLBACK_STATUS_SENDREQUEST_COMPLETE:
+            if (WinHttpReceiveResponse (request, NULL))
+                return;
 
-                raddi::log::error (raddi::component::main, 0x25, context->url, "start");
-                break;
+            this->report (raddi::log::level::error, 0x25, this->url, "start");
+            break;
 
-            case WINHTTP_CALLBACK_STATUS_HEADERS_AVAILABLE:
-                size = sizeof buffer;
-                if (WinHttpQueryHeaders (request, WINHTTP_QUERY_STATUS_CODE, NULL, buffer, &size, WINHTTP_NO_HEADER_INDEX)) {
+        case WINHTTP_CALLBACK_STATUS_HEADERS_AVAILABLE:
+            size = sizeof buffer;
+            if (WinHttpQueryHeaders (request, WINHTTP_QUERY_STATUS_CODE, NULL, buffer, &size, WINHTTP_NO_HEADER_INDEX)) {
 
-                    auto status = std::wcstoul (reinterpret_cast <const wchar_t *> (buffer), nullptr, 10);
-                    if (status == 200) {
-                        if (WinHttpQueryDataAvailable (request, NULL))
-                            return;
-
-                        raddi::log::error (raddi::component::main, 0x25, context->url, "query");
-                    } else
-                        raddi::log::error (raddi::component::main, 0x27, context->url, status);
-                } else
-                    raddi::log::error (raddi::component::main, 0x25, context->url, "header");
-                break;
-
-            // NOTE: I really need to stop writing code like this
-
-            case WINHTTP_CALLBACK_STATUS_READ_COMPLETE:
-                if (size) {
-                    if (const auto data = static_cast <char *> (data_)) {
-                        data [size] = '\0';
-
-                        // separate rows
-                        auto p = data;
-                        while (*p) {
-
-                            // left trim
-                            while (std::isspace (*p)) {
-                                ++p;
-                            }
-                            if (*p) {
-
-                                // find new-line or end of the file
-                                std::ptrdiff_t length;
-                                if (auto e = std::strchr (p, L'\n')) {
-                                    length = e - p;
-                                } else {
-                                    length = &data [size] - p;
-                                }
-
-                                // right trim
-                                while (length && std::isspace (p [length - 1])) {
-                                    --length;
-                                }
-
-                                wchar_t ip [48];
-                                if (auto n = MultiByteToWideChar (CP_UTF8, 0, p, (int) length,
-                                                                  ip, sizeof ip / sizeof ip [0] - 1)) {
-                                    if (n >= 9) {
-                                        ip [n] = L'\0';
-
-                                        SOCKADDR_INET address;
-                                        if (StringToAddress (address, ip)) {
-                                            DownloadedCallback (address);
-                                            raddi::log::event (raddi::component::main, 0x20, context->url, ip);
-                                        } else {
-                                            raddi::log::error (raddi::component::main, 0x28, context->url, ip);
-                                        }
-                                    }
-                                }
-                                p += length;
-                            }
-                        }
-                    }
-
-            case WINHTTP_CALLBACK_STATUS_DATA_AVAILABLE:
-                    if (WinHttpReadData (request, buffer, sizeof buffer - 1, NULL))
+                auto status = std::wcstoul (reinterpret_cast <const wchar_t *> (buffer), nullptr, 10);
+                if (status == 200) {
+                    if (WinHttpQueryDataAvailable (request, NULL))
                         return;
 
-                    raddi::log::error (raddi::component::main, 0x25, context->url, "read");
+                    this->report (raddi::log::level::error, 0x25, this->url, "query");
+                } else
+                    this->report (raddi::log::level::error, 0x27, this->url, status);
+            } else
+                this->report (raddi::log::level::error, 0x25, this->url, "header");
+            break;
+
+        // NOTE: I really need to stop writing code like this
+
+        case WINHTTP_CALLBACK_STATUS_READ_COMPLETE:
+            if (size) {
+                data [size] = '\0';
+
+                // separate rows
+                if (auto p = data) {
+                    while (*p) {
+
+                        // left trim
+                        while (std::isspace (*p)) {
+                            ++p;
+                        }
+                        if (*p) {
+
+                            // find new-line or end of the file
+                            std::ptrdiff_t length;
+                            if (auto e = std::strchr (p, L'\n')) {
+                                length = e - p;
+                            } else {
+                                length = &data [size] - p;
+                            }
+
+                            // right trim
+                            while (length && std::isspace (p [length - 1])) {
+                                --length;
+                            }
+                            p [length] = '\0';
+
+                            if (!this->callback->downloaded (this->url, p))
+                                goto cancelled;
+
+                            p += length;
+                        }
+                    }
                 }
-        }
-        WinHttpCloseHandle (request);
-        WinHttpCloseHandle (context->connection);
-        delete context;
+
+        case WINHTTP_CALLBACK_STATUS_DATA_AVAILABLE:
+                if (WinHttpReadData (request, buffer, sizeof buffer - 1, NULL))
+                    return;
+
+                this->report (raddi::log::level::error, 0x25, this->url, "read");
+            }
     }
+
+cancelled:
+    WinHttpCloseHandle (request);
+    delete this;
+}
+
+Download::Context::~Context () {
+    WinHttpCloseHandle (this->connection);
 }
--- node/download.h
+++ node/download.h
@@ -3,15 +3,72 @@
 
 #include <winsock2.h>
 #include <ws2ipdef.h>
+#include <winhttp.h>
+
+#include "../common/log.h"
 
 // Download
 //  - facility for HTTP-downloading bootstrap text files containing IP addresses
-//  - TODO: class, inherit log::provider, 
+//
+class Download
+    : raddi::log::provider <raddi::component::main> {
 
-bool InitializeDownload (const wchar_t * proxy, const wchar_t * user_agent);
-void TerminateDownload ();
-void Download (wchar_t * url);
-void DownloadedCallback (const SOCKADDR_INET &);
+    HINTERNET internet = NULL;
+
+public:
+    Download (const wchar_t * proxy, const wchar_t * user_agent);
+    ~Download ();
+
+    // Callback
+    //  - classes that need to receive downloaded text implement this interface
+    //
+    class Callback {
+    public:
+
+        // downloaded
+        //  - override is called for every truncated ASCII line from the downloaded file 'url'
+        //  - return: true to continue downloading additional lines for this file
+        //            false to stop
+        //
+        virtual bool downloaded (const std::wstring & url, const char * line) = 0;
+    };
+
+    // download
+    //  - starts asynchronous download of 'url' file,
+    //    to be asynchronously reported to 'callback' class
+    //  - NOTE: the action is destructive on the string provided in 'url' (TODO: fix?)
+    //  - returns: true when download successfully started, false otherwise
+    //
+    bool download (wchar_t * url, Callback * callback);
+
+private:
+
+    // Context
+    //  - context of a single download in progress
+    //
+    class Context
+        : raddi::log::provider <raddi::component::main> {
+
+        HINTERNET    connection;
+        Callback *   callback;
+        std::wstring url;
+
+        void HttpHandler (HINTERNET, DWORD, char *, DWORD);
+
+    public:
+        Context (HINTERNET connection, Callback * callback, const std::wstring & url)
+            : provider ("download")
+            , connection (connection)
+            , callback (callback)
+            , url (url) {};
+
+        ~Context ();
+
+        static void WINAPI HttpHandlerFwd (HINTERNET request, DWORD_PTR context, DWORD code, LPVOID data, DWORD size) {
+            reinterpret_cast <Download::Context *> (context)->HttpHandler (request, code, static_cast <char *> (data), size);
+        }
+    };
+};
 
 #endif
 
